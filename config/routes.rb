26d9f6ea6673027c3b6855bffbf11db16312dc34
Clement Duval <clement.duval44@hotmail.fr>
2023-11-14 17:04:30 +0100

    2nd ex

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :tasks
+  # index
+  # get "tasks", to: "tasks#index"
+  # create
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # post "tasks", to: "tasks#create"
+  # show
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # edit
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+  # destroy
+  # delete "tasks/:id", to: "tasks#destroy"
 end
